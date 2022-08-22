d6cbb0866f7ca320d97a24c9ef57ffd20ace8d01
akshatV21 <a.vishwakarma2110@gmail.com>
2022-08-22 09:03:24 +0530

    guildMemberRemove event added

---
+++ src/events/memberRemove-event.js
@@ -0,0 +1,10 @@
+const event = {
+  name: "guildMembeRemove",
+  once: false,
+  execute(member) {
+    const validChannelID = "1010931699682644030"
+    member.guild.channels.cache.get(validChannelID).send(`${member.user} has left the server!!`)
+  },
+}
+
+module.exports = event
