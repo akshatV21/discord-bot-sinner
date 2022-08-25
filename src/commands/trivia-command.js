aa5ef04233c052d13b265e1172f647420c97d571
akshatV21 <a.vishwakarma2110@gmail.com>
2022-08-25 10:08:15 +0530

    trivia command code modified

--- src/commands/trivia-command.js
+++ src/commands/trivia-command.js
@@ -6,55 +6,54 @@ const { getRandomlyPlacedOptions } = require("../helpers/command-helpers")
 const command = {
   data: new SlashCommandBuilder().setName("trivia").setDescription("asks the user trivia questions!!"),
   async execute(interaction) {
+    const validChannelID = process.env.TRIVIA_CH_ID
+    if (validChannelID !== interaction.channelId) {
+      interaction.reply({ content: "❌ CANNOT USE THIS HERE ❌", ephemeral: true })
+      return
+    }
+
+    const channel = interaction.guild.channels.cache.get(validChannelID)
+
+    // fetching the question
+    const response = await axios.get(process.env.TRIVIA_API_URL)
+    const trivia = await response.data.results[0]
+
+    // destructuring the question
+    const { question, correct_answer, incorrect_answers } = trivia
+    const options = getRandomlyPlacedOptions([...incorrect_answers, correct_answer])
+    const correctOption = options.findIndex(option => option === correct_answer)
+
+    // creating the question embed
+    const triviaEmbed = new EmbedBuilder()
+      .setTitle("TRIVIA")
+      .setDescription("Reply with the correct answer's option number only!!")
+      .setFields({ name: "Question", value: `${question}` })
+      .setFooter({ text: "You have 15 seconds to answer this question!!" })
+
+    // adding the options to the embed field
+    options.forEach((option, index) => {
+      triviaEmbed.addFields({ name: `${index + 1}) ${option}`, value: "----------------------" })
+    })
+
+    // sending the embed field
+    await interaction.reply({ embeds: [triviaEmbed], fetchReply: true })
+
+    // getting the user's answer/response
+    const filter = msg => msg.author.id === interaction.user.id
+
+    // catching the timeout error
     try {
-      const validChannelID = process.env.TRIVIA_CH_ID
-      if (validChannelID !== interaction.channelId) {
-        interaction.reply({ content: "❌ CANNOT USE THIS HERE ❌", ephemeral: true })
-        return
-      }
-
-      const channel = interaction.guild.channels.cache.get(validChannelID)
-
-      // fetching the question
-      const response = await axios.get(process.env.TRIVIA_API_URL)
-      const trivia = await response.data.results[0]
-
-      // destructuring the question
-      const { question, correct_answer, incorrect_answers } = trivia
-      const options = getRandomlyPlacedOptions([...incorrect_answers, correct_answer])
-      const correctOption = options.findIndex(option => option === correct_answer)
-
-      // creating the question embed
-      const triviaEmbed = new EmbedBuilder()
-        .setTitle("TRIVIA")
-        .setDescription("Reply with the correct answer's option number only!!")
-        .setFields({ name: "Question", value: `${question}` })
-        .setFooter({ text: "You have 15 seconds to answer this question!!" })
-
-      // adding the options to the embed field
-      options.forEach((option, index) => {
-        triviaEmbed.addFields({ name: `${index + 1}) ${option}`, value: "----------------------" })
-      })
-
-      // sending the embed field
-      await interaction.reply({ embeds: [triviaEmbed], fetchReply: true })
-
-      // getting the user's answer/response
-      const filter = msg => msg.author.id === interaction.user.id
-      const userResponse = await channel.awaitMessages({ filter, time: 10000, maxMatches: 1, error: ["time", "maxMatches"] })
-      const userAnswer = Number(userResponse.first().content) - 1
-
-      if (userAnswer === correctOption) {
-        interaction.followUp(`${interaction.user} You got the answer right!!`)
-      } else {
-        interaction.followUp(`${interaction.user} You missed the chance buddy!!\nCorrect answer was: ${correct_answer}`)
-      }
+      const userResponse = await channel.awaitMessages({ filter, time: 15000, max: 1, errors: ["time"] })
     } catch (error) {
-      console.log(`[ERROR] - ${error}`)
-      interaction.followUp({
-        content: "Internal server error!!",
-        ephemeral: true,
-      })
+      interaction.followUp({ content: `${interaction.user} ⌛ TIMEOUT!! ⌛` })
+      return
+    }
+    const userAnswer = Number(userResponse.first().content) - 1
+
+    if (userAnswer === correctOption) {
+      interaction.followUp(`${interaction.user} 👍 You got the answer right!! 👍`)
+    } else {
+      interaction.followUp(`${interaction.user} ❌ You missed the chance buddy!! ❌\nCorrect answer was: ${correct_answer}`)
     }
   },
 }
